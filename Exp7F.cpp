d57bbb96045dd9690149892a78e6215b29fdb532
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-31 09:17:49 +0530

    Create Exp7F.cpp

---
+++ Exp7F.cpp
@@ -0,0 +1,24 @@
+// EXP 7F : Concatenation of a string
+// Archisha Chatterjee
+// PRN : 24070123021
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    string a = "Archisha";
+    string b = " Chatterjee";
+    string str1 = a+b;
+    cout << str1;
+    
+
+    return 0;
+}
+
+/* 
+OUTPUT :
+
+Archisha Chatterjee
+*/
