897dee545eebec98232bdfdcab82cf206d4186f1
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-31 09:16:19 +0530

    Create Exp7E.cpp

---
+++ Exp7E.cpp
@@ -0,0 +1,25 @@
+// EXP 7E : Take a string as an input and display it
+// Archisha Chatterjee
+// PRN : 24070123021
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    string a;
+    cout << "Enter string : ";
+    cin >> a;
+    cout << "Echo string : " << a;
+    
+
+    return 0;
+}
+
+/*
+OUTPUT :
+
+Enter string : hello!
+Echo string : hello!
+*/
